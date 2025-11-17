6440b364e89296d519db5ad49adca40b8764a17a
billy <gaetan.begue13@gmail.com>
2025-11-17 16:20:35 +0100

    cors option

--- Cafeterie/src/middlewares/cors.js
+++ Cafeterie/src/middlewares/cors.js
@@ -5,19 +5,11 @@ const cors = require('cors');
 const allowedOrigins = [
   process.env.FRONTEND_URL || 'http://localhost:5173',
   'https://cafeterie-ksqrk3gwd-gaetan1303s-projects.vercel.app',
-  'https://cafeterie.vercel.app',"*"
+  'https://cafeterie.vercel.app',
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Autoriser les requêtes sans origin (ex: Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true, // Autorise toutes les origines temporairement
   credentials: true,
 };
 
